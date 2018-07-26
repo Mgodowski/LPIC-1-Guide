99a3e795bfab15bf848c5f35849ce7eb5a262bcd
Mgodowski <martin.godowski@plentymarkets.com>
2018-07-26 19:28:49 +0200

    Update

--- scripts/TTFB.sh
+++ scripts/TTFB.sh
@@ -1,6 +1,10 @@
 #! /bin/bash/
 
-read -p 'Bitte die Website eingeben:_' $1 
-
-curl -o /dev/null -w "Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} \n"  
+read -p "Bitte URL eingeben:" \$1
+curl -o /dev/null \
+     -H 'Cache-Control: no-cache' \
+     -s \
+     -w "Connect: %{time_connect} \\n
+	    TTFB: %{time_starttransfer} \\n
+	    Total time: %{time_total} \\n" \\
 echo "$1"
