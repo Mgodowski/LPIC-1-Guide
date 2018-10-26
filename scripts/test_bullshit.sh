aa50cd0d2ddb67219655bea71a2356ae888b5529
Mgodowski <martin.godowski@plentymarkets.com>
2018-10-26 12:14:56 +0200

    Test bullshit

---
+++ scripts/test_bullshit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#IF-ELSE-Anweisungen
+
+if grep "$1" /etc/passwd > /dev/null 2>$1
+then
+echo "Ja den Benutzer $1 gibt es"
+
+#Weitere Kommandos eingeben
+else
+echo "Nein den Benutzer $1 gibt es nicht"
+
+#Weitere Kommandos eingeben
+fi
